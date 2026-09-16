ebfa97ab553c08c996a328138cbe282dc7b80521
Matt Eva <matteweva@gmail.com>
2026-09-16 16:33:33 -0700

    removing unecessary typescript flag
    created sample component

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import MyComponent from "./component";
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(
+  <div>
+    <h2>Hello from React!</h2>
+    <MyComponent />
+  </div>,
+);
